0e6c4eeb4aa5ccce968cb7f33d3ddea889f61441
UltraMKF12 <harcos212@gmail.com>
2022-10-18 07:56:49 +0300

    Create folder for alg. 2

---
+++ algoritmika/2. Feladatcsomag/HaromDijazott/HaromDijazott.cpp
@@ -0,0 +1,17 @@
+// Széri József - sjim2307
+// HaromDijazott
+// KIJELENTES
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    freopen("bemenet.txt", "r", stdin);
+    freopen("kimenet.txt", "w", stdout);
+
+    int alma;
+    cin >> alma;
+    cout << "Hello World!";
+    return 0;
+}
