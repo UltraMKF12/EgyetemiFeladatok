792007422fd0a46908b6d96e4d8334333bddc7ee
UltraMKF12 <harcos212@gmail.com>
2022-12-02 15:39:14 +0200

    3 elkezdve

--- algoritmika/5. Feladatcsomag/Muveletek.cpp
+++ algoritmika/5. Feladatcsomag/Muveletek.cpp
@@ -8,15 +8,90 @@ minden lépésben legyen egész szám! A műveleteket nem a matematikából isme
 operátorprecedencia szerint végezzük, hanem megjelenésük sorrendjében.
 */
 
-#include <iostream> 
+#include <iostream>
+#include <vector>
 using namespace std;
+
+void beolvasas(int &n, vector<int> &szamok, int &eredmeny)
+{
+    cin >> n;
+    szamok.resize(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> szamok[i];
+    }
+    cin >> eredmeny;
+}
+
+int jel_konverzio(int szam1, int szam2, char jel)
+{
+    switch (jel)
+    {
+    case '+':
+        return szam1 + szam2;
+    case '-':
+        return szam1 - szam2;
+    case '*':
+        return szam1 * szam2;
+    case '/':
+        return szam1 / szam2;
+
+    default:
+        return 0;
+    }
+}
+
+void kiir(const vector<int> &szamok, const vector<char> &jelenlegi_jelek, int eredmeny)
+{
+    cout << eredmeny << "=";
+    int hossz = jelenlegi_jelek.size();
+    for (int i = 0; i < hossz; i++)
+    {
+        cout << szamok[i] << jelenlegi_jelek[i];
+    }
+    cout << szamok[hossz];
+}
+
+void muveletek(int n, const vector<int> &szamok, vector<char> &jelenlegi_jelek, const vector<char> &jelek, int eredmeny, int melyseg, int jelenlegi_eredmeny, int &darab)
+{
+    if(melyseg > n-1)
+    {
+        if(eredmeny == jelenlegi_eredmeny)
+        {
+            kiir(szamok, jelenlegi_jelek, eredmeny);
+            darab++;
+        }
+    }
+    else
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            char jel = jelek[i];
+            jelenlegi_jelek[melyseg] = jel;
+            
+        }
+        
+    }
+}
 
 int main()
 {
     freopen("bemenet.txt", "r", stdin);
-    freopen("kimenet.txt", "w", stdout);
+    // freopen("kimenet.txt", "w", stdout);
 
+    int n, eredmeny;
+    vector<int> szamok;
+    beolvasas(n, szamok, eredmeny);
 
+    vector<char> jelek = {'+', '-', '*', '/'};
+    vector<char> jelenlegi_jelek(n-1, ' ');
+    int darab = 0;
+    muveletek(n, szamok, jelenlegi_jelek, jelek, eredmeny, 0, 0, darab);
+
+    if(darab == 0)
+    {
+        cout << -1;
+    }
 
     return 0;
 }
