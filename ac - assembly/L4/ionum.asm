9b15c8cf67b72467789fe7f845e848fd0b240a99
UltraMKF12 <harcos212@gmail.com>
2022-12-17 19:23:39 +0200

    ionum kezdetek proba int

--- ac - assembly/L4/ionum.asm
+++ ac - assembly/L4/ionum.asm
@@ -27,87 +27,160 @@ global main
 section .text
 
 ReadInt:
+    push    esi
+    push    edi
+    push    ebx
+    push    ecx
+    push    edx
+
+    xor     eax, eax
+    xor     ebx, ebx
+    xor     edx, edx
+
+    mov     esi, esp
+    sub     esi, 500
+    mov     ecx, 256
+    call    ReadStr
+
+    mov     ecx, 10
+    ;esi - szám-szöveg
+    ;ecx - szorzó
+    ;eax - számjegy
+    ;ebx - segédszám
+
+    .new_digit:
+        lodsb
+
+        cmp     al, 0
+        je      .end
+
+        ;Minusz jel
+        cmp     al, '-'
+        je      .minus
+
+        ;Ha nem számjegy
+        cmp     al, '0'
+        jl      .error
+        cmp     al, '9'
+        jg      .error
+
+
+        imul    ebx, ecx
+        jo      .error
+
+        sub     eax, '0'
+
+        cmp     edx, 0
+        je      .positive
+        cmp     edx, 1
+        je      .negative
+
+        jmp     .error
+
+     .minus:
+        cmp     edx, 1
+        je      .error
+        mov     edx, 1
+        jmp     .new_digit
+    
+
+    .negative:
+        sub     ebx, eax
+        jo      .error
+        jmp     .new_digit
+    
+    .positive: 
+        add     ebx, eax
+        jo      .error
+        jmp     .new_digit
+
+
+    .error:
+        stc
 
     .end:
+        pop     edx
+        pop     ecx
+        pop     ebx
+        pop     edi
+        pop     esi
+
         ret
 
-        
+
 WriteInt:
 
     .end:
         ret
 
-        
+
 ReadInt64:
 
     .end:
         ret
 
-        
+
 WriteInt64:
 
     .end:
         ret
 
-        
+
 ReadBin:
 
     .end:
         ret
 
-        
+
 WriteBin:
 
     .end:
         ret
 
-        
+
 ReadBin64:
 
     .end:
         ret
 
-        
+
 WriteBin64:
 
     .end:
         ret
 
-        
+
 ReadHex:
 
     .end:
         ret
 
-        
+
 WriteHex:
 
     .end:
         ret
 
-        
+
 ReadHex64:
 
     .end:
         ret
 
-        
+
 WriteHex64:
 
     .end:
         ret
 
-        
+
 
 main:
     mov     esi, str_a_text
     call    WriteStr
 
-    mov     ecx, 20
-    mov     esi, str_a
-    call    ReadLnStr
+    call    ReadInt
 
-    call    WriteStr
     ret
     
 section .data
