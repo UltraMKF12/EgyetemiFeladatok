a37de62c8b8988b31f008c21433a28b7a2e7a466
UltraMKF12 <harcos212@gmail.com>
2022-12-18 14:02:09 +0200

    c cuccok

--- c alapok/sjim2307_L4_2.c
+++ c alapok/sjim2307_L4_2.c
@@ -4,90 +4,130 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void nullazos_realloc(int **matrix, int regi_meret, int uj_meret)
-{
-    int **temp = (int **)realloc(matrix, uj_meret * sizeof(int *));
-    if(temp == NULL)
-    {
-        printf("HIBA!");
-    }
-    for (int i = regi_meret; i < uj_meret; i++)
-    {
-        temp[i] = (int *)realloc(temp[i], uj_meret * sizeof(int));
-    }
-    matrix = temp;
 
-    //Lenullazni az uj elemeket.
-    for (int i = regi_meret; i < uj_meret; i++)
+int **matrix_lefoglalas(int sor, int oszlop)
+{
+    int **matrix = (int **) calloc(sor, sizeof(int *));
+    for (int i = 0; i < sor; i++)
     {
-        for (int j = regi_meret; j < uj_meret; j++)
+        matrix[i] = (int *) calloc(oszlop, sizeof(int));
+    }
+
+    return matrix;
+}
+
+void matrix_felszabaditas(int **matrix, int sor)
+{
+    for(int i = 0; i < sor; i++)
+    {
+        free(matrix[i]);
+    }
+    free(matrix);
+}
+
+int **matrix_ujrafoglalas_nullazas(int **matrix, int sor, int oszlop, int uj_sor, int uj_oszlop)
+{
+    matrix = (int **) realloc(matrix, uj_sor * sizeof(int *));
+    for (int i = 0; i < sor; i++)
+    {
+        matrix[i] = (int *) realloc(matrix[i], uj_oszlop * sizeof(int));
+    }
+
+    //Nullázni az új foglalással az új sorokat
+    for (int i = sor; i < uj_sor; i++)
+    {
+        matrix[i] = (int *) calloc(uj_oszlop, sizeof(int));
+    }
+
+    //Nullázni az új adatokat (a meglévő sorokban új oszlopokat)
+    for (int i = 0; i < sor; i++)
+    {
+        for (int j = oszlop; j < uj_oszlop; j++)
         {
             matrix[i][j] = 0;
         }
     }
+
+    return matrix;
 }
 
-void network_beolvasas(int **network, int *n)
-{   
-    //Kezdetleges lefoglalos networknek (calloc lenullazza)
-    network = (int **)calloc(*n, sizeof(int *));
-    for (int i = 0; i < *n; i++)
+int *matrix_1D_lefoglalas(int sor, int oszlop)
+{
+    int *matrix = (int*) calloc(sor*oszlop, sizeof(int));
+    return matrix;
+}
+
+int *matrix_1D_ujrafoglalas_nullazas(int *matrix, int sor, int oszlop, int uj_sor, int uj_oszlop)
+{
+    matrix = (int *) realloc(matrix, uj_sor*uj_oszlop * sizeof(int));
+
+    //Nullázni az új adatokat (a meglévő sorokban új oszlopokat)
+    for (int i = 0; i < sor; i++)
     {
-        network[i] = (int *)calloc(*n, sizeof(int));
+        for (int j = oszlop; j < uj_oszlop; j++)
+        {
+            matrix[i*ujszor + j] = 0;
+        }
     }
-    
-    int elozo_n;
+
+    //Nullázni az új foglalással az új sorokat
+    for (int i = sor; i < uj_sor; i++)
+    {
+        matrix[i] = (int *) calloc(uj_oszlop, sizeof(int));
+    }
+
+
+    return matrix;
+}
+
+void matrix_kiiras(int **matrix, int sor, int oszlop)
+{
+    for (int i = 0; i < sor; i++)
+    {
+        for (int j = 0; j < oszlop; j++)
+        {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int **network_beolvasas(int *n)
+{   
+    int temp_n = *n;
+    int elozo_n = temp_n;
+    int **matrix = matrix_lefoglalas(temp_n, temp_n);
     int kiindulasi_pont;
     int veg_pont;
     int suly;
+
     FILE *network_text;
     network_text = fopen("network.txt", "r");
-    while(fscanf(network_text, "%d %d %d", &kiindulasi_pont, &veg_pont, &suly) != EOF)
+    while(fscanf(network_text, "%d %d %d", &kiindulasi_pont, &veg_pont, &suly) == 3)
     {
-        printf("%d %d %d\n", kiindulasi_pont, veg_pont, suly);
-        //Ha barmelyik pont nagyobb mint n, akkor ujra kell foglalni.
-        if(kiindulasi_pont > *n || veg_pont > *n)
-        {   
-            elozo_n = *n;
-            *n = (kiindulasi_pont > veg_pont) ? kiindulasi_pont : veg_pont;
-            printf("Elozo n = %d ||| jelenlegi n = %d\n", elozo_n, *n);
-            // nullazos_realloc(network, elozo_n, *n);
+        if(kiindulasi_pont > temp_n || veg_pont > temp_n)
+        {
+            elozo_n = temp_n;
+            temp_n = (kiindulasi_pont >= veg_pont) ? kiindulasi_pont+1 : veg_pont+1;
+            matrix = matrix_ujrafoglalas_nullazas(matrix, elozo_n, elozo_n, temp_n, temp_n);
         }
+
+        matrix[kiindulasi_pont][veg_pont] = suly;
+        matrix[veg_pont][kiindulasi_pont] = suly;
     }
     
-    fclose(network_text);
-}
 
-void kiindulo_csomopont()
-{
-    FILE *input = fopen("input.txt", "r");
-    int alma;
-    int pista;
-    int kecske;
-    fscanf(input, "%d %d %d", &alma, &pista, &kecske);
-    printf("%d %d %d", alma, pista, kecske);
-    fscanf(input, "%d %d %d", &alma, &pista, &kecske);
-    printf("%d %d %d", alma, pista, kecske);
+    fclose(network_text);
+    *n = temp_n;
+    return matrix;
 }
 
 int main()
 {
-
-    int **network;
     int n = 1;
+    int **matrix = network_beolvasas(&n);
 
-    // kiindulo_csomopont();
-    network_beolvasas(network, &n);
-
-    printf("%d", n);
-
-
-    //Felszabaditas
-    for(int i = 0; i < n; i++)
-    {
-        free(network[i]);
-    }
-    free(network);
-
+    matrix_kiiras(matrix, n, n);
+    matrix_felszabaditas(matrix, n);
     return 0;
 }
