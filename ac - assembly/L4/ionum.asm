6fadf7c7fe5624d965c54fba685bc6bd9d32b5e6
UltraMKF12 <harcos212@gmail.com>
2023-01-10 09:45:11 +0200

    hex betevese, nem megy meg
    + megoldani binaris beolvasasnak a hiba uzenet ne legyen 32-edik nel

--- ac - assembly/L4/ionum.asm
+++ ac - assembly/L4/ionum.asm
@@ -226,14 +226,198 @@ WriteBin64:
 
 
 ReadHex:
+    ;AL                                   0000 0000 - FF
+    ;AX                         0000 0000 0000 0000 - FFFF
+    ;EAX    0000 0000 0000 0000 0000 0000 0000 0000 - FFFF FFFF
+    
+    ;A fügvény EAX-be olvassa be az értéket.
+
+    push    ebx                             ;Kell az ebx szam felepitesre
+    push    ecx                             ;Számláló, max 8 szám lehet beírva
+    xor     ebx, ebx
+    xor     ecx, ecx
+
+    .bemenet_szoveg:
+        ;Szöveg kiiratás
+        push    esi
+        mov     esi, str_hexa_prefix
+        call    WriteStr
+        pop     esi
+
+
+    .uj_karakter:
+        xor     eax, eax                    ;Le kell nullázni hibák elkerülésére
+        call    mio_readchar
+
+        ;ENTER - eseten véget ér a beolvasás
+        cmp     al, 13
+        je      .end
+
+        ;BACKSPACE
+        cmp     al, 8
+        je      .backspace
+
+        ;Karakter kiiratasa
+        call    mio_writechar
+
+        ;MAXIMÁLIS HOSSZ
+        cmp     ecx, 8                      ;Ha 8 karakter hosszú a hexa számjegy, akkor már nem lehet többet beleírni.
+        je      .hiba
+
+        ;--------Szám Ellenrőzések--------
+        ;(ASCII) Legkissebtől kezdbe, legnagyobbig haladva, megnézzük, hogy benne van e a felső határban.
+        ;Ha igen, akkor le kell tesztelni, hogy az alsó határban is benne van, különben hiba.
+        ;---------------------------------
+        cmp     al, '9'                     ;57
+        jle     .szamjegy
+        cmp     al, 'F'                     ;70
+        jle     .nagy_betu
+        cmp     al, 'f'                     ;102
+        jle     .kis_betu
+
+        jmp     .hiba                       ;Minden más esetben helytelen karakter.
+
+
+    .szamjegy:
+        cmp     al, '0'
+        jl      .hiba
+
+        shl     ebx, 4                      ; 4-el eltoljuk az ebx-et
+        sub     eax, '0'                    ; megszerezzük a számjegy értékét
+        add     ebx, eax                    ; hozzáadjuk ebx-hez, garantáltan 4 biten elfér
+
+        inc     ecx                         ;Karakter számláló növelése
+        jmp     .uj_karakter
+
+    
+    .nagy_betu:
+        cmp     al, 'A'
+        jl      .hiba
+
+        shl     ebx, 4                      ; 4-el eltoljuk az ebx-et
+        sub     eax, 'A'                    ; megszerezzük a betü értékét
+        add     eax, 10                     ; hozzá kell adni 10-et, mert 10 a legkisebb szám (A)
+        add     ebx, eax                    ; hozzáadjuk ebx-hez, garantáltan 4 biten elfér
+
+        inc     ecx                         ;Karakter számláló növelése
+        jmp     .uj_karakter
+    
+
+    .kis_betu:
+        cmp     al, 'a'
+        jl      .hiba
+
+        shl     ebx, 4                      ; 4-el eltoljuk az ebx-et
+        sub     eax, 'a'                    ; megszerezzük a betü értékét
+        add     eax, 10                     ; hozzá kell adni 10-et, mert 10 a legkisebb szám (a)
+        add     ebx, eax                    ; hozzáadjuk ebx-hez, garantáltan 4 biten elfér
+
+        inc     ecx                         ;Karakter számláló növelése
+        jmp     .uj_karakter
+
+    
+    .backspace:
+        cmp     ecx, 0                      ;Ne tudjuk kitörölni olyan szöveget, amit nem mi irtunk
+        je      .uj_karakter
+        
+        mov     al, 8                       ;Nem volt kiírva a backspace, hogy ne menjünk vissza, ha nem lenne szabad
+        call    mio_writechar
+        mov     al, ' '
+        call    mio_writechar
+        mov     al, 8
+        call    mio_writechar
+
+        dec     ecx                         ;Csökken a karakter számláló
+        shr     ebx, 4                      ;Visszavisszük a beolvasható poziciót
+
+        ;Uj karaktert kell bekerni.
+        jmp    .uj_karakter
+
+
+    .hiba:
+        ;Betesszük a számot eax-be.
+        mov     eax, ebx
+
+        ;Visszaszerezzük az értékekek stackből.
+        pop     ecx
+        pop     ebx
+
+        stc
+        ret
+
 
     .end:
+        ;Betesszük a számot eax-be.
+        mov     eax, ebx
+
+        ;Visszaszerezzük az értékekek stackből.
+        pop     ecx
+        pop     ebx
+
         ret
 
 
 WriteHex:
+    ;A fügvény EAX-ből írja ki a számot hexadecimális formában
+
+    ;Szöveg kiírása
+    push    esi
+    mov     esi, str_hexa_prefix
+    call    WriteStr
+    pop     esi
+
+    ;Értékek elmentése
+    push    eax                             ;eax eredeti értékének elmentése
+    push    ebx                             ;hexa szamjegyek? tárolása
+    push    ecx                             ;Számláló
+    push    edx
+
+    mov     ecx, 8                          ;8 karaktert kell beolvasni
+
+    .felbontas:
+        mov     ebx, 0xF                    ;.... 0000 1111
+        and     ebx, eax                    ;Megszerezzük a számot az utolsó 4 bitről.
+        push    ebx                         ;Elmentjük a számot
+
+        shr     eax, 4                      ;Eltoljuk 4-el az eax-et, hogy megkapjuk a következő számot
+        loop    .felbontas
+
+
+    mov     ecx, 8                          ;8 karaktert kell kiírni
+    .kiiras:
+        pop     eax                         ;Kiszedjük a számot a stackből.
+
+        cmp     eax, 10                     
+        jl      .szamjegy                   ;Ha ebx < 10(1010) akkor számjegyről van szó.
+        jmp     .betu                       ;Ellenkező esetben betűről
+
+
+    .szamjegy:
+        add     eax, '0'                    ; Számjegy ASCII kóddá alakítása
+        call    mio_writechar
+
+        jmp     .kiiras_veg
+    
+
+    .betu:
+        sub     eax, 10                     ;Ahhoz, hogy A-F karaktert kapjunk, ki kell vonni 10-et. A=10
+        add     eax, 'A'
+        call    mio_writechar
+
+        jmp     .kiiras_veg
+
+
+    .kiiras_veg:
+        loop    .kiiras
+
 
     .end:
+        ;Visszaszerezzük az értékekek stackből.
+        pop     edx
+        pop     ecx
+        pop     ebx
+        pop     eax
+
         ret
 
 
@@ -261,6 +445,7 @@ main:
     
 section .data
     str_a_text db "A = ", 0
+    str_hexa_prefix db "0x", 0
 
 section .bss
     str_a   resb 256
