312757e0e149be7a3ff1edb99261a0bf11f168bc
UltraMKF12 <harcos212@gmail.com>
2023-01-22 17:37:24 +0200

    L8 - 1,2

---
+++ algoritmika/8. Feladatcsomag/MatrixokSzorzasa.cpp
@@ -0,0 +1,17 @@
+// Széri József - sjim2307
+//MatrixokSzorzasa
+/*
+*/
+
+#include <iostream> 
+using namespace std;
+
+int main()
+{
+    freopen("bemenet.txt", "r", stdin);
+    freopen("kimenet.txt", "w", stdout);
+
+
+
+    return 0;
+}
