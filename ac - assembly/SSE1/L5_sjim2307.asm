1bbf67eaac466d19223ca471463d42a2a335e987
UltraMKF12 <harcos212@gmail.com>
2023-02-14 20:09:20 +0200

    exponencialis olvaso megy, float kiiro nem

--- ac - assembly/SSE1/L5_sjim2307.asm
+++ ac - assembly/SSE1/L5_sjim2307.asm
@@ -376,6 +376,123 @@ Kettevalasztas:
         ret
 
 
+KettevalasztasEX:
+    ;Return eax and ebx with integers, edx 0 or 1(positive/negative)
+    xor     eax, eax
+    xor     ebx, ebx
+    xor     ecx, ecx    ;Ide kerul E erteke
+    xor     edx, edx    ;Elojel index
+    xor     edi, edi    
+    
+
+    ;Elojel teszt
+    lodsb
+    cmp     al, "-"
+    je      .negativ
+    cmp     al, "+"
+    je      .pozitiv
+    
+    cmp     al, "9"
+    jg      .hiba
+    cmp     al, "0"
+    jl      .hiba
+
+    jmp     .szamjegy
+
+    .negativ:
+        mov     edx, 1
+        jmp     .folyatas
+    .pozitiv:
+        mov     edx, 0
+        jmp     .folyatas
+    .szamjegy:
+        mov     edx, 0
+        sub     esi, 1
+        jmp     .folyatas
+
+
+    .folyatas:
+    mov     edi, temp1
+    .darabolas:
+        xor     eax, eax
+        lodsb
+
+        cmp     al, 0
+        je      .end
+
+        cmp     al, "."
+        je      .pont
+
+        cmp     al, "e"
+        je      .e_betu
+
+        cmp     al, "E"
+        je      .e_betu
+
+        cmp     al, "9"
+        jg      .hiba
+        cmp     al, "0"
+        jl      .hiba
+        
+        stosb
+        mov     [edi + 1], byte 0
+
+        jmp     .darabolas
+
+    .pont:
+        mov     edi, temp2
+        jmp     .darabolas
+
+    .e_betu:
+        mov     edi, temp4
+        xor     eax, eax
+        lodsb
+        
+        cmp     al, "-"
+        je      .jeles
+        cmp     al, "+"
+        je      .jeles
+
+        dec     esi
+        jmp     .e_betu_loop
+
+        .jeles:
+            stosb
+            
+    .e_betu_loop:
+        xor     eax, eax
+        lodsb
+
+        cmp     al, 0
+        je      .end
+
+        cmp     al, "9"
+        jg      .hiba
+        cmp     al, "0"
+        jl      .hiba
+
+        stosb
+        mov     [edi + 1], byte 0
+
+        jmp     .e_betu_loop
+
+    .hiba:
+        stc
+        ret
+    .end:
+        mov     esi, temp4
+        call    ReadInt
+        mov     ecx, eax
+
+        mov     esi, temp2
+        call    ReadInt
+        mov     ebx, eax
+        mov     esi, temp1
+        call    ReadInt
+        call    NewLine
+        ret
+
+
 ;_______[Sima float]_______
 ReadFloat:
     pushad
@@ -458,15 +575,81 @@ WriteFloat:
 
 ;_______[EX float]_______
 ReadFloatEX:
-    ret
+    pushad
+
+    ;Beolvasni a számot
+    ;+- első karakter után állítani a +- dolgot.
+    ;. karakternél kettéválaszatni, két számjegyet kinyerni, majd floatos cuccként összeadni
+    mov     esi, temp3
+    mov     ecx, 256
+    call    ReadStr
+    call    KettevalasztasEX
+    ;EAX = elso resz
+    ;EBX = masodik resz
+    ;ECX = E erteke
+    ;EDX = elojel
+
+    ;Egesz resz
+    xorps       xmm0, xmm0
+    cvtsi2ss    xmm0, eax
+
+    ;Tort resz
+    xorps       xmm1, xmm1
+    cvtsi2ss    xmm1, ebx
+
+    movss       xmm2, [float_div]
+
+    .nullazas:
+        cvttss2si   eax, xmm1
+        cmp         eax, 0
+        je          .e_ezes
+
+        divss       xmm1, xmm2
+        jmp         .nullazas
+    
+    .e_ezes:
+        addss   xmm0, xmm1
+        cmp     edx, 1
+        je      .negativos
+        jmp     .nem_negativos
+        .negativos:
+            movss   xmm2, [float_negativ]
+            mulss   xmm0, xmm2
+        .nem_negativos:
+        movss     xmm1, [float_div]
+        cmp     ecx, 0
+        jl      .e_ezes_loop_negativ
+        jmp     .e_ezes_loop_pozitiv
+    .e_ezes_loop_pozitiv:
+        cmp     ecx, 0
+        je      .end
+
+        dec     ecx
+        mulss   xmm0, xmm1
+
+        jmp     .e_ezes_loop_pozitiv
+    .e_ezes_loop_negativ:
+        call    io_writeflt
+        call    NewLine
+        cmp     ecx, 0
+        je      .end
+
+        inc     ecx
+        divss   xmm0, xmm1
+
+        jmp     .e_ezes_loop_negativ
+
+    .end:
+        popad
+        ret
 
 WriteFloatEX:
     ret
-;------------------------
 
+;------------------------
 main:
     ;E(a,b,c,d) = a * (d - b) - sqrt(a + c / 2)
-    call    ReadFloat
+    call    ReadFloatEX
     call    NewLine
     call    WriteFloat
     ret
@@ -488,6 +671,7 @@ section .bss
     temp1 resb 256
     temp2 resb 256
     temp3 resb 256
+    temp4 resb 256
     a   resd 1
     b   resd 1
     c   resd 1
