9931ab6fed80c22b2d8dc062b7eaac45373d3762
Széri József <harcos212@gmail.com>
2023-04-18 12:37:43 +0300

    graf 4 default fajlok

---
+++ 2. felev/graf/lab4/L04_sjim2307_2.cpp
@@ -0,0 +1,18 @@
+//Széri József - 514/2 - sjim2307
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct csomopont
+{
+    int id;
+    bool volt_mar;
+    bool szinezett;
+    vector<csomopont*> kifele;
+};
+
+int main()
+{
+
+    return 0;
+}
