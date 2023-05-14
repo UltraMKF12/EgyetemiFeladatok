b04dfac413aa0a4d65651185f0f59e51a255b11e
Széri József <harcos212@gmail.com>
2023-05-14 04:28:06 +0300

    lab 4_2 done

--- 2. felev/graf/lab4/L04_sjim2307_2.cpp
+++ 2. felev/graf/lab4/L04_sjim2307_2.cpp
@@ -1,6 +1,7 @@
 //Széri József - 514/2 - sjim2307
 #include <iostream>
 #include <vector>
+#include <limits.h>
 using namespace std;
 
 struct el;
@@ -10,7 +11,7 @@ struct csomopont
     int ertek;
     bool latogatott;
     csomopont* elozo;
-    vector<el> kifele;
+    // vector<el> kifele;
 };
 
 struct el
@@ -20,7 +21,7 @@ struct el
     csomopont* kovetkezo;
 };
 
-void graf_beolvas(int m, vector<csomopont> &csomopontok)
+void graf_beolvas(int m, vector<csomopont>& csomopontok, vector<el>& elek)
 {
     int elso, masodik, suly;
     for (int i = 0; i < m; i++)
@@ -31,13 +32,104 @@ void graf_beolvas(int m, vector<csomopont> &csomopontok)
         vonal.suly = suly;
         vonal.jelenlegi = &csomopontok[elso];
         vonal.kovetkezo = &csomopontok[masodik];
-        //Iranyitott
-        csomopontok[elso].kifele.push_back(vonal);
+
+        // //Iranyitott
+        // csomopontok[elso].kifele.push_back(vonal);
+
+        // Itt csak élek kellenek
+        elek.push_back(vonal);
     }
+}
+
+void init_s(vector<csomopont>& csomopontok, int kezdopont)
+{
+    csomopontok[kezdopont].ertek = 0;
+    csomopontok[kezdopont].latogatott = true;
+}
+
+void relax(el &vonal)
+{
+    if(vonal.jelenlegi->latogatott && 
+       vonal.kovetkezo->ertek > vonal.jelenlegi->ertek + vonal.suly)
+    {
+        vonal.kovetkezo->ertek = vonal.jelenlegi->ertek + vonal.suly;
+        vonal.kovetkezo->elozo = vonal.jelenlegi;
+        
+        vonal.kovetkezo->latogatott = true;
+    }
+}
+
+bool Bellman_Ford(vector<csomopont> &csomopontok, vector<el>& elek, int kezdopont)
+{
+    init_s(csomopontok, kezdopont);
+    for (int i = 0; i < csomopontok.size()-2; i++) // 0-tol szamozzuk, 1-tol hasznaljuk ezert -2
+    {
+        for (int i = 0; i < elek.size(); i++)
+        {
+            relax(elek[i]);
+        }
+    }
+
+    for (int i = 0; i < elek.size(); i++)
+    {
+        if(elek[i].jelenlegi->latogatott && elek[i].kovetkezo->latogatott && 
+           elek[i].kovetkezo->ertek > elek[i].jelenlegi->ertek + elek[i].suly)
+        {
+            return false;
+        }
+    }
+    
+    return true;
 }
 
 int main()
 {
+    freopen("graf.in", "r", stdin);
+    int n, m, kezdopont;
+    cin >> n >> m >> kezdopont;
+    vector<csomopont> csomopontok(n+1);
+    for (int i = 0; i < csomopontok.size(); i++)
+    {
+        csomopontok[i] = {i, INT_MAX, false, nullptr};
+    }
+    vector<el> elek;
+    graf_beolvas(m, csomopontok, elek);
+
+    if(Bellman_Ford(csomopontok, elek, kezdopont))
+    {
+        for (int i = 1; i < csomopontok.size(); i++)
+        {
+            if(i != kezdopont)
+            {
+                if(!csomopontok[i].latogatott)
+                {
+                    cout << "A legrovidebb ut hossza " << i << " fele: NEM ELERHETO" << endl;
+                    cout << "A legrovidebb ut " << i << " fele: NEM ELERHETO" ;
+                }
+                else
+                {
+                    cout << "A legrovidebb ut hossza " << i << " fele: " << csomopontok[i].ertek << endl;
+                    cout << "A legrovidebb ut " << i << " fele: ";
+                    
+                    csomopont* jelenlegi = &csomopontok[i];
+                    while (jelenlegi->id != kezdopont)
+                    {
+                        cout << jelenlegi->id << " ";
+                        if(jelenlegi->elozo != nullptr)
+                        {
+                            jelenlegi = jelenlegi->elozo;
+                        }
+                    }
+                    cout << jelenlegi->id; // Kiirni a kezdopontot.
+                }
+                cout << endl << endl;
+            }
+        }
+    }
+    else
+    {
+        cout << "Van negativ kor";
+    }
 
     return 0;
 }
